42b8201ad97b3c80e4488e937c005673443989c5
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-08-31 13:21:17 -0600

    Add files via upload

---
+++ Methodology.py
@@ -0,0 +1,30 @@
+
+person = "Connor"
+
+for i in range(10):
+    print(i)
+
+list = ['one', 'two', 'three']
+
+dictionary = {
+    'name': 'Connor',
+    'age': 14
+}
+
+def is_even_number(number):
+    if number % 2 == 0:
+        print('This is an even number!')
+    else:
+        print('This is an odd number!')
+
+is_even_number(13)
+
+MAIN_CHARACTER = "Mario"
+
+main_character = "Luigi"
+
+camelCase = "This is how you write Camel Case"
+
+this_is_snake_case = "That is how you write Snake Case"
+
+ThisIsPascalCase = "That is how you write Pascal Case"
