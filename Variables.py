0cd0eb6d83c86d031b429bcaae4fe4ea71d5c25d
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-09-05 13:14:24 -0600

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,23 @@
+my_name = "Connor James Andrson Barrera"
+test_score = 10
+was_my_lunch_tasty = True
+my_b_day_classes = [
+ 
+    "Kedge",
+    "Concert Band",
+    "Acedemic Decathalon",
+    "Computer Programing",
+    "Web Development"
+
+]
+
+
+total = 0
+
+def add_numbers(Number1,Number2):
+    return(Number1 + Number2)
+
+
+print(add_numbers(int(input("First Number ")),int(input("Second Number "))))
+
+print(f"Hi, my name is {my_name}. I just scored {test_score} on my last test. My classes today are {my_b_day_classes}.")
