668db172942d9272e8546cfb5d4eb63dfa561ca6
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-09-07 13:30:29 -0600

    Add files via upload

---
+++ Concepts.py
@@ -0,0 +1,53 @@
+my_name = "Connor Anderson"
+
+def give_a_name():
+    a_name = "Kyle"
+    print(a_name)
+    #local variable only exists in function
+
+print(my_name)
+#global exists everywhere
+
+my_name = input("What is your name?")
+#Input: info given by users
+
+print(f"Hello {my_name}, nice to meet you!")
+#Output: info given by computers
+
+name = "Jerrard"
+#strings are words
+
+integer = 1
+#int's are whole numbers
+
+floatt = 0.1
+#float's are decimal places
+
+my_boolean = True
+
+declaration = ""
+initialization = "hello world"
+
+add = 1 + 2
+subtract = 2 - 3
+divide = 2/1
+multiply = 2 * 3
+modulo = 2 % 1
+exponent = 2 ** 10
+
+print(modulo)
+
+statement = 4 > 2
+statement1 = 4 >= 2
+statement2 = 4 <= 2
+statement3 = 4 == 2
+statement3 = 4 != 2
+
+print statement(3)
+
+
+message = 4 > 2 and 33 < 43
+message 2 = 4 < 2 and 33 < 43
+x = True
+
+print(not x)
