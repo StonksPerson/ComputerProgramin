1694f23f346bcfc702e35a082b8c00e95b665fa5
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-09-08 11:24:47 -0600

    Add files via upload

--- Concepts.py
+++ Concepts.py
@@ -43,11 +43,45 @@ statement2 = 4 <= 2
 statement3 = 4 == 2
 statement3 = 4 != 2
 
-print statement(3)
+print (statement3)
 
 
 message = 4 > 2 and 33 < 43
-message 2 = 4 < 2 and 33 < 43
+message2 = 4 < 2 and 33 < 43
+message3 = 4 < 2 or 33 < 43
 x = True
 
+y = 10
+x = y >= 10
+
+
 print(not x)
+
+#Order of operations: Parenthesis Exponent Multiplication Modulus Division Addition Subtraction
+
+z = 7 // 2
+#The double division is a integer division which binds the number to a whole number instead of a float
+print(z)
+t = 7 / 2
+print(t)
+
+def my_function():
+    print('I am so great!')
+
+my_function()
+
+def add(num1, num2):
+    print(num1 + num2)
+
+add(1,2)
+
+my_name = "Mr Hunt"
+words = my_name.split(' ')
+print(words)
+
+def Subtraction(num1, num2):
+    return(num1 - num2)
+
+diference = Subtraction(4,5)
+
+print(diference)
