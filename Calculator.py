44789ecc32e566cec0e2e13d811b8b8119de72d5
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-09-12 13:02:25 -0600

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,14 @@
 num1 = int(input("First Number: "))
 num2 = int(input("Second Number: "))
-which = input("What Operation: ")
+which = input("What Operation? please type one of the following: add, subtract, multiply, divide ")
 
-if(which == "plus" or which == "add"):
+if which == "add":
     answer = num1 + num2
-elif(which == "minus" or which == "subtract"):
+elif which == "subtract":
     answer = num1 - num2
-elif(which == "times" or which == "mulitply"):
+elif which == "mulitply":
     answer = num1 * num2
-elif(which == "divide"):
-    answer = num1 / num2
+elif which == "divide":
+    answer = num1 // num2
 
 print(f"{num1} {which} {num2} equals {answer}")
-
-
-    
