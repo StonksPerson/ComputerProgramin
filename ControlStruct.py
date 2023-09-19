4e33283d00057ca18d91ac1ebb479c32fca5f495
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-09-19 13:31:14 -0600

    Add files via upload

---
+++ ControlStruct.py
@@ -0,0 +1,26 @@
+car = "blue"
+car_make = "Tesla"
+
+if car == "Blue" or car == "blue":
+    if car_make == "Tesla":
+        print("My car is blue Tesla")
+    else:
+        print("My car is blue")
+elif car == "Red":
+    print("My car is a ferrari")
+else: 
+    print("I just have a bike")
+
+# for loop
+for i in range(1,10):
+    print(i)
+
+sports = ["soccer","Basketball","Hockey","Football"]
+print(sports)
+
+for item in sports:
+    print(item)
+
+i=1
+while i<6 do:
+    i += 1
