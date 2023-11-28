0be52c04a6efd38542533befeff31bbfc9a345ce
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-11-28 13:23:02 -0700

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -4,38 +4,73 @@ import math
 words = open("words.txt")
 words = words.readlines()
 
+newWord = ""
+guessed_letters = ""
+guess_word = ""
 word = words[math.floor(random.random() * 267751.0)]
+for i in range(len(word) - 1):
+    newWord = newWord + word[i]
+word = newWord
 revealedWord = ""
 loopWord = ""
 win = False
 tries = 0
 maxTries = 10
+guessed_words = []
+lastLoop = ""
 
 for i in range(len(word)):
     revealedWord = revealedWord + "_ "
 
 while win == False:
 
-    if(tries > maxTries):
+    guessed = 1
+
+    if(tries >= maxTries):
         break
+
+    guess_word = ""
+    lastLoop = revealedWord
+
     loopWord = ""
-    print(revealedWord)
-    guess = input("Type you letter guess, if you think you know what the word is feel free to guess. : ")
+    guess = input(f"Type you letter guess. you have {maxTries - tries} tries left: ")
     guess = guess.upper()
 
-    if(len(guess) > 1):
-        if(guess + " " == word):
-            win = True
-            break
+    if(guessed_words.count(guess) > 0):
+        guessed = 0
     else:
-        if(word.find(guess) != -1):
-            for i in range(len(word) - 1):
-                if(word[i] == guess or revealedWord.find(word[i]) > -1):
-                    loopWord = loopWord + word[i] + " "
-                else: loopWord = loopWord + "_ "
-            revealedWord = loopWord
-        else: tries += 1
+        guessed_words.append(guess)
+   
+    for i in range(len(word)):
+        if(word[i] == guess or revealedWord.find(word[i]) > -1):
+            
+            loopWord = loopWord + word[i] + " "
+            guess_word = guess_word + word[i]
+        else: loopWord = loopWord + "_ "
 
+    revealedWord = loopWord
+    
+    if(revealedWord != lastLoop):
+        print("Correct guess :)")
+        tries -= 1
+
+    if(guessed == 0):
+        print("You already guessed that!")
+    tries += guessed
+    guessed_letters = ""
+    for i in range(len(guessed_words)):
+        guessed_letters = guessed_letters + guessed_words[i] + ", "
+
+    if(guess_word == word):
+        win = True
+        break
+
+    print(f"So far you have guessed guessed {guessed_letters}")
+    print(revealedWord)
+    print(len(guess_word))
+    print(len(word))
+    print(word)
+    
 
 if(win == True):
     print(f'You WIN. The word was: {word}')
